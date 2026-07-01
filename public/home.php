8d95f5654549be7a77a37d26433f8ebb14e596bd
sojiro takagi <soujirou.takagi@gmail.com>
2026-07-01 20:23:47 +0900

    #16 ホームページの作成

---
+++ public/home.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>瀧本図書館</title>
+</head>
+<body>
+    <h1>瀧本図書館</h1>
+
+    <nav>
+        <ul>
+            <li><a href="books.php">本一覧</a></li>
+            <li><a href="borrow.php">貸し出し</a></li>
+            <li><a href="return.php">返却</a></li>
+            <li><a href="students.php">学生管理</a></li>
+            <li><a href="loans.php">貸し出し履歴</a></li>
+            <li><a href="admin-books.php">本の管理</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
